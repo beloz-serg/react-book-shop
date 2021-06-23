88766bee50da4a111c1a7ab3b7023cddf28dad51
Белозеров Сергей <belozyorovsergey@yandex.ru>
2021-06-23 18:51:22 +0300

    used webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+};
